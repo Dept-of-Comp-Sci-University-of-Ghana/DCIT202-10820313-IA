ba1cfc7e472115dc7592aa84647ba7d2f566a837
louis <lallotey@st.ug.edu.gh>
2021-11-15 21:33:05 +0000

    addition of images and icons

---
+++ assets/images/images.js
@@ -0,0 +1,62 @@
+import homebg from '../iphonebg.jpg'
+import pic from '../profilePic.jpg'
+import iphone13 from '../iphone13.jpg'
+import iphone13pro from '../iphone13pro.png'
+import iphone13promax from '../iphone13promax.png'
+import iphone12 from '../iphone12.png'
+import iphone12pro from '../iphone12pro.png'
+import iphone12promax from '../iphone12promax.png'
+import iphone12mini from '../iphone12mini.png'
+import iphone11 from '../iphone11.jpg'
+import iphone11pro from '../iphone11pro.png'
+import iphone11promax from '../iphone11promax.png'
+import iphonex from '../iphoneX.png'
+import iphonexr from '../iphoneXr.jpg'
+import iphonexs from '../iphoneXs.png'
+import iphonexsmax from '../iphoneXsmax.jpg'
+import iphone8 from '../iphone8.jpg'
+import iphone8plus from '../iphone8plus.jpg'
+import iphone7 from '../iphone7.jpg'
+import iphone7plus from '../iphone7plus.jpg'
+import iphone6 from '../iphone6.jpg'
+import iphone6plus from '../iphone6plus.jpg'
+import iphone6s from '../iphone6s.jpg'
+import iphone6splus from '../iphone6splus.jpg'
+
+
+
+
+const images = {
+    backgroundImage: homebg,
+    profilePic: pic,
+    iphone13 : iphone13,
+    iphone13pro : iphone13pro,
+    iphone13promax : iphone13promax,
+    iphone12 : iphone12,
+    iphone12pro : iphone12pro,
+    iphone12promax : iphone12promax,
+    iphone12mini : iphone12mini,
+    iphone11 : iphone11,
+    iphone11pro : iphone11pro,
+    iphone11promax : iphone11promax,
+    iphonex : iphonex,
+    iphonexr : iphonexr,
+    iphonexs : iphonexs,
+    iphonexsmax : iphonexsmax,
+    iphone8 : iphone8,
+    iphone8plus : iphone8plus,
+    iphone7 : iphone7,
+    iphone7plus :iphone7plus,
+    iphone6 : iphone6,
+    iphone6plus : iphone6plus,
+    iphone6s : iphone6s,
+    iphone6splus : iphone6splus,
+
+
+
+
+
+
+}
+
+export default images
